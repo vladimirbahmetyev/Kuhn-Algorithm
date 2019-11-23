42e0c7ed8d06abd76d494bc12f671dba294cda76
Vladimir <vladimir240298perm@gmail.com>
2019-11-24 01:36:32 +0300

    Create project and write Kuhn Alg

---
+++ Kuhn Algorithm/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kuhn_Algorithm
+{
+    
+    internal class KuhnAlgorithm
+    {
+        private int[] _mt;
+        private bool[] _used;
+
+        public KuhnAlgorithm()
+        {
+            _used = new bool[0];
+            _mt = new int[0];
+        }
+
+        private bool try_kuhn(int v, List<int>[] g)
+        {
+            if (_used[v]) return false;
+            _used[v] = true;
+            for (var i = 0; i < g[v].Count; ++i)
+            {
+                var to = g[v][i];
+                if (_mt[to] != -1 && !try_kuhn(_mt[to], g)) continue;
+                _mt[to] = v;
+                return true;
+            }
+
+            return false;
+        }
+        
+        public List<(int, int)> findMaxMatchingInGraph(int n, int k, List<int>[] reb)
+        {
+            _mt = Enumerable.Repeat(-1, k).ToArray();
+            for (var i = 0; i < n; ++i)
+            {
+                _used = Enumerable.Repeat(false, n).ToArray();
+                try_kuhn(i, reb);
+            }
+            
+            var result = new List<(int,int)>();
+            for (var i = 0; i < k; ++i)
+                if (_mt[i] != -1)
+                {
+                    result.Add((_mt[i] + 1, i + 1));
+                }
+            return result;
+        }
+        
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
---
+++ Kuhn Algorithm/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Kuhn_Algorithm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kuhn_Algorithm")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("64F2D72B-1C3F-49D5-8D0B-BDB5BD0BD1CB")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
